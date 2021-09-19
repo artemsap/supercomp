cebb395f05f419b45c75c7ae9e2bdd9cd791dfa0
Artem Sapozhnikov <62465465+artemsap@users.noreply.github.com>
2021-09-19 18:12:03 +0300

    Рефакторинг

--- main.cpp
+++ main.cpp
@@ -3,58 +3,53 @@
 #include <stdlib.h>
 #include <ctime>
 
-#define THREADS 3
-#define MAX_NUM 100
-#define MIN_NUM -100
+#define THREADS 5
+#define MAX_NUM 1000
+#define MIN_NUM -1000
 
 void blas_dgemm(int M, int N, int K, double *A, double *B, double *C);
-void print_matrix(int num_col, int num_str, double *matrix);
-void generate_matrix(int num_col, int num_str, double *matrix);
+void print_matrix(int num_col, int num_rows, double *matrix);
+void generate_matrix(int num_col, int num_rows, double *matrix);
 
 int main (void)
 {
   std::srand(time(0));
-  int max_num = 5000;
+  int max_num = 50;
 
-  //int M = std::rand()%max_num + 1; //Количество столбцов в матрице А и строк в матрице B
-  //int N = std::rand()%max_num + 1; //Количество строк в матрице А
-  //int K = std::rand()%max_num + 1; //Количество столбцов в матрице B
-
-  int M = 1000;
-  int N = 2000;
-  int K = 1500;
+  int M = std::rand()%max_num + 1; //Number of columns in A matrix and rows in B matrix
+  int N = std::rand()%max_num + 1; //Number of rows in A matrix
+  int K = std::rand()%max_num + 1; //Number of columns in A matrix
 
   double *A = new double[M*N];
   double *B = new double[M*K];
   double *C = new double[N*K];
 
-  double start = clock(); // засекаем время старта
+  // Time before matrix generation
+  double start = clock(); 
 
+  //Matrix generation
   generate_matrix(M, N, A);
   generate_matrix(K, M, B);
 
-  double end = clock(); // засекаем время окончания
+  // Time after matrix generation
+  double end = clock(); 
 
-  double t1 = (end - start) / CLOCKS_PER_SEC;
-
-  //printf("MATRIX A\n");
-  //print_matrix(M, N, A);
-
-  //printf("MATRIX B\n");
-  //print_matrix(K, M, B);
+  // Total time of matrix generation
+  double t1 = (end - start) / CLOCKS_PER_SEC; 
 
   memset(C, 0, sizeof(double)*N*K);
 
-  start = clock(); // засекаем время старта
+  // Time before matrix multiplication
+  start = clock(); 
 
+  // Matrix multiplications
   blas_dgemm(M, N, K, A, B, C);
 
-  end = clock(); // засекаем время окончания
+  // Time after matrix multiplication
+  end = clock(); 
   
+  // Total time of matrix multiplication
   double t = (end - start) / CLOCKS_PER_SEC;
-
-  //printf("MATRIX C\n");
-  //print_matrix(K, N, C);
 
   printf("Time of matrix generation=%f \n", t1);
   printf("Time of matrix multiplication=%f \n", t);
@@ -62,6 +57,7 @@ int main (void)
   delete[] A;
   delete[] B;
   delete[] C;
+
   return 0;
 }
 
@@ -77,9 +73,9 @@ void blas_dgemm(int M, int N, int K, double *A, double *B, double *C)
   }
 }
 
-void print_matrix(int num_col, int num_str, double *matrix)
+void print_matrix(int num_col, int num_rows, double *matrix)
 {
-  for(int i=0; i<num_str; i++)
+  for(int i=0; i<num_rows; i++)
   {
     for(int j=0; j<num_col; j++)
       printf("%5.2f \t", matrix[i*num_col +j]);
@@ -87,13 +83,13 @@ void print_matrix(int num_col, int num_str, double *matrix)
   }
 }
 
-void generate_matrix(int num_col, int num_str, double *matrix)
+void generate_matrix(int num_col, int num_rows, double *matrix)
 {
   #pragma omp parallel num_threads(THREADS)
   {
     #pragma omp for
     for(int i=0; i<num_col; i++)
-      for(int j=0; j<num_str; j++)
-        matrix[i*num_str +j] = (MIN_NUM + std::rand()%(MAX_NUM-MIN_NUM+1))/100.0;
+      for(int j=0; j<num_rows; j++)
+        matrix[i*num_rows +j] = (MIN_NUM + std::rand()%(MAX_NUM-MIN_NUM+1))/100.0;
   }
 }
